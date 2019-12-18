37024bb74e483319dd840559c5665404782bf6f4
terol_notePC <purazumateroru@gmail.com>
2019-12-18 23:36:04 +0900

    remake Bayesian>main()

--- Regression/Bayesian_Optimization/Bayesian_Optimization.py
+++ Regression/Bayesian_Optimization/Bayesian_Optimization.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
 import kernel_matrix as km
-
+import matplotlib.pyplot as plt
 
 # class Bayesian_optimization:
 #     def __init__(self, n = 2):
@@ -17,31 +17,72 @@ import kernel_matrix as km
 
 #         return x, y, A
 
+def truef(x):
+    # return np.sin(10*x[0][:]) + np.sin(10*x[1])
+    return np.sin(10*x[0][:])
 
-def read_data(n = 2):
+def read_data(n = 1):
     p = 4
 
     x = np.random.rand(n, p)
-    # xx = x[0] + x[1]
-    # A = np.insert( x, 0, [1]* p, axis = 0).T
     max_r = 0.1
     min_r = -0.1
-    y = ((max_r - min_r) * np.random.rand(p) + min_r) + np.sin(10*x[0]) + np.sin(10*x[1])
+    y = ((max_r - min_r) * np.random.rand(p) + min_r) + np.sin(10*x)#  + np.sin(10*x[1])
 
-    return x.T, y
+    return x.T, y.reshape(-1, 1)
+
+def test_x(n = 1):
+    p = 3
+    # x = np.linspace(-1, 1, p).reshape(n, p)
+    x = np.random.rand(n, p)
+    return x.T
+
+def hyper_P():
+    shita_1, shita_2, shita_3 = 1, 0.4, 0.1
+    return [shita_1, shita_2, shita_3]
 
 if __name__ == "__main__":
     # Bo = Bayesian_optimization()
     # x, z, A = Bo.read_data()
 
-    n = 2
+    n = 1
     x, y = read_data(n)
+    x_test = test_x(n)
+    shita = hyper_P()
+    print (x)
+    print (y)
+    print (x_test)
+    K = km.kernel_function.Kernel_matrix(x, x, shita, "GK", "Diagonal")
+    K_inv = np.linalg.inv(K)
 
-    K = km.kernel_function.Kernel_matrix(x, x, len(x), len(x), "GK")
+    k_star = km.kernel_function.Kernel_matrix(x, x_test, shita, "GK", "Non_Diagonal")
+    k_starstar = km.kernel_function.Kernel_matrix(x_test, x_test, shita, "GK", "Diagonal")
+    
+    print (K)
+    print (k_star)
+    print (k_starstar)
 
-    yy = np.linalg.solve(K, y)
+    mu = k_star.T @ K_inv @ y
+    var = k_starstar - k_star @ K_inv @ k_star.T
 
     print (x)
     print (y)
     print (K)
-    print (yy)
+    print (k_star)
+
+    print ("mu >> ")
+    print (mu)
+    print ("var >> ")
+    print (var)
+
+    plt.figure(figsize=(12,8))
+    plt.title('The result')
+    plt.fill_between(x_test.flatten(), mu -np.sqrt(var), mu +np.sqrt(var))
+    plt.plot(x_test.flatten(), mu , color='red', label='predicted_mean')
+    plt.scatter(x.flatten(), y.flatten(), label='traindata')
+    plt.plot(x_test.flatten(), truef(x_test.flatten()), label='true_label', color='purple')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
--- Regression/Bayesian_Optimization/kernel_matrix/kernel_function.py
+++ Regression/Bayesian_Optimization/kernel_matrix/kernel_function.py
@@ -1,15 +1,28 @@
 # coding: utf-8
+import numpy as np
 import kernel_matrix as km
 
-def hyper_P():
-    shita_1, shita_2, shita_3 = 1, 0.4, 0.1
-    return [shita_1, shita_2, shita_3]
 
-def Kernel_matrix(x, x_, N, M, kernel_type = "GK"):
+
+def Kernel_matrix(x, x_, shita, kernel_type = "GK", matrix_type = "Diagonal"):
     if (kernel_type == "GK"):
-        shita = hyper_P()
+        if (matrix_type == "Diagonal"):
+            N = len(x)
+            M = len(x_)
+            K = [[km.Gauss_kernel.Gauss_kernel(x[i][:], x_[j][:], i, j, shita) for j in range(M)] for i in range(N)]
+            
         
-        K = [[km.Gauss_kernel.Gauss_kernel(x[i][:], x_[j][:], i, j, shita) for j in range(M)] for i in range(N)]
-        
-        return K
+        elif (matrix_type == "Non_Diagonal"):
+            N = len(x)
+            M = len(x_)
+            K = [[km.Gauss_kernel.Gauss_kernel(x[i][:], x_[j][:], -1, j, shita) for j in range(M)] for i in range(N)]
+            
 
+    return np.array(K)
+
+# def Kernel_vector(x, x_, M, shita, kernel_type = "GK"):
+#     if (kernel_type == "GK"):
+        
+#         K = [km.Gauss_kernel.Gauss_kernel(x[j][:], x_, -1, j, shita) for j in range(M)]
+        
+#         return np.array(K)
